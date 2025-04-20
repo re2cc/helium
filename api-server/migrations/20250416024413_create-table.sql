82e32bb97db6e52a4b33e1c81621081542baf9fa
re2cc <42478705+re2cc@users.noreply.github.com>
2025-04-19 18:39:02 -0600

    feat: create initial database schema for product management

---
+++ api-server/migrations/20250416024413_create-table.sql
@@ -0,0 +1,57 @@
+-- SQLITE --
+
+CREATE TABLE product (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    variation_enabled BOOLEAN NOT NULL,
+    variation_name TEXT,
+);
+
+CREATE TABLE product_variation (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    barcode TEXT NOT NULL UNIQUE,
+    current_sell_price DECIMAL(10,2) NOT NULL,
+    current_inventary DECIMAL(10,2) NOT NULL,
+    product_id INTEGER NOT NULL FOREIGN KEY REFERENCES product(id),
+);
+
+CREATE TABLE provider (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+);
+
+CREATE TABLE buy_history (
+    id INTEGER NOT NULL PRIMARY KEY,
+    provider_id INTEGER FOREIGN KEY REFERENCES provider(id),
+    date DATETIME NOT NULL,
+)
+
+CREATE TABLE product_buy_history (
+    id INTEGER NOT NULL PRIMARY KEY,
+    product_variation_id INTEGER NOT NULL FOREIGN KEY REFERENCES product_variation_id(id),
+    buy_history_id INTEGER NOT NULL FOREIGN KEY REFERENCES buy_history(id),
+    unit_value DECIMAL(10,2) NOT NULL,
+    quantity DECIMAL(10,2) NOT NULL,
+);
+
+CREATE TABLE client (
+    id INTEGER NOT NULL PRIMARY KEY,
+    phone_number TEXT,
+    email TEXT,
+    name TEXT,
+);
+
+CREATE TABLE sell_history (
+    id INTEGER NOT NULL PRIMARY KEY,
+    client_id INTEGER FOREIGN KEY REFERENCES client(id),
+    date DATETIME NOT NULL,
+)
+
+CREATE TABLE product_sell_history (
+    id INTEGER NOT NULL PRIMARY KEY,
+    product_variation_id INTEGER NOT NULL FOREIGN KEY REFERENCES product_variation_id(id),
+    sell_history_id INTEGER NOT NULL FOREIGN KEY REFERENCES sell_history(id),
+    unit_value DECIMAL(10,2) NOT NULL,
+    quantity DECIMAL(10,2) NOT NULL,
+);
